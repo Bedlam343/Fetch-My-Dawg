60132fc968ca0f5c0e390c0a79cfe1e28a36566f
bedlam343 <jagik22@gmail.com>
2025-02-02 13:06:06 -0800

    sort by field

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent } from 'react';
 import Icons from 'src/icons';
+import { DEFAULT_SORT_FIELD, SortField } from 'src/utils/constants';
 import { SortDirection } from 'src/utils/types';
 
 type FilterProps = {
@@ -8,6 +9,7 @@ type FilterProps = {
   selectedBreed: string;
   onSelectBreed: (breed: string) => void;
   onSetSortDirection: (direc: SortDirection) => void;
+  onSetSortField: (field: SortField) => void;
 };
 
 const Filters = ({
@@ -15,6 +17,7 @@ const Filters = ({
   sortDirection,
   onSelectBreed,
   onSetSortDirection,
+  onSetSortField,
 }: FilterProps) => {
   const handleBreedSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const breed = e.target.value;
@@ -27,7 +30,7 @@ const Filters = ({
   return (
     <div
       className="flex w-full mb-[50px] border-b-[1px]
-  border-stone-400 border-dotted py-3 gap-[30px] items-center"
+    border-stone-400 border-dotted py-3 gap-[40px] items-center"
     >
       <div className="flex">
         <Icons.Filter height={30} width={30} fill="white" />
@@ -36,7 +39,7 @@ const Filters = ({
 
       <select
         className="text-center w-[200px] bg-black bg-opacity-50 text-slate-100
-      text-ellipsis rounded-lg border-[1px] border-stone-600 h-[40px]"
+        text-ellipsis rounded-lg border-[1px] border-stone-600 h-[40px]"
         defaultValue="All Breeds"
         onChange={handleBreedSelectChange}
       >
@@ -52,32 +55,57 @@ const Filters = ({
         ))}
       </select>
 
-      <div
-        className="flex h-[40px] border-2 border-stone-200 items-center
-        rounded-sm"
-      >
+      <div className="flex items-center gap-[10px]">
+        <div>
+          <p className="text-stone-200">Sort By:</p>
+        </div>
+
+        <select
+          className="text-center w-[100px] bg-black bg-opacity-50 text-slate-100
+        text-ellipsis rounded-lg border-[1px] border-stone-600 h-[40px]"
+          defaultValue={DEFAULT_SORT_FIELD}
+          onChange={(e) =>
+            onSetSortField(SortField[e.target.value as keyof typeof SortField])
+          }
+        >
+          {Object.keys(SortField).map((field) => (
+            <option
+              key={field}
+              className="text-sm w-[200px] px-2 overflow-ellipsis
+        text-stone-900 bg-white"
+            >
+              {field}
+            </option>
+          ))}
+        </select>
+
         <div
-          onClick={() => onSetSortDirection('asc')}
-          className={`h-full hover:cursor-pointer w-[100px] hover:text-stone-700
+          className="flex h-[35px] border-2 border-stone-200 items-center
+        rounded-sm"
+        >
+          <div
+            onClick={() => onSetSortDirection('asc')}
+            className={`h-full hover:cursor-pointer w-[75px] hover:text-stone-700
           hover:bg-stone-200 flex items-center justify-center ${
             sortDirection === 'asc'
               ? 'bg-stone-200 text-stone-700'
               : ' text-stone-200'
           }`}
-        >
-          Asc
-        </div>
-        <div className="h-full w-[1px] bg-white" />
-        <div
-          onClick={() => onSetSortDirection('desc')}
-          className={`h-full hover:cursor-pointer w-[100px]
+          >
+            Asc
+          </div>
+          <div className="h-full w-[1px] bg-white" />
+          <div
+            onClick={() => onSetSortDirection('desc')}
+            className={`h-full hover:cursor-pointer w-[75px]
           flex items-center justify-center ${
             sortDirection === 'desc'
               ? 'bg-stone-200 text-stone-700'
               : 'text-stone-200 hover:text-stone-700 hover:bg-stone-200'
           }`}
-        >
-          Desc
+          >
+            Desc
+          </div>
         </div>
       </div>
     </div>
