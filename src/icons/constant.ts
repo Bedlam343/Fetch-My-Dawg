4f52d73d0e4748c0d5dfebbf9f8cc4a3b33bc599
bedlam343 <jagik22@gmail.com>
2025-02-04 08:01:27 -0800

    delete unsued file

--- src/icons/constant.ts
+++
@@ -1,9 +0,0 @@
-import { IconProps } from './types';
-
-export const applyClass = (props: IconProps) => {
-  return `${
-    props.disabled
-      ? 'hover:cursor-not-allowed opacity-50'
-      : 'hover:cursor-pointer '
-  }`;
-};
